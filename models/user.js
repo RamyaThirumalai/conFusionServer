04591bd54b9612f0112e87055620609e46b739cd
SWATI DANDEKAR <swati.dandekar@in.ibm.com>
2019-12-23 18:45:36 +0530

    Added basic user athuntication policy

---
+++ models/user.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var User = new schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required : true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = mongoose.model('User', User);
